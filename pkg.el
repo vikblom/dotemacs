f9149aa5ec5549734f87c00a29f33c27acc88c0a
Viktor Blomqvist <veblomqvist@gmail.com>
2019-04-21 15:29:33 +0200

    Lexical binding, refactors, new preferred theme.

--- pkg.el
+++ pkg.el
@@ -18,8 +18,6 @@
 
 ;; Custom keyword that conditionally disables
 (add-to-list 'use-package-keywords ':onlyif)
-;; Only use first arguemnt
 (defun use-package-normalize/:onlyif (_name _keyword _arg) (car _arg))
-;; Pass on rest only if (eval arg)
 (defun use-package-handler/:onlyif (name _keyword _arg rest state)
   (use-package-process-keywords name (if (eval _arg) rest) state))
