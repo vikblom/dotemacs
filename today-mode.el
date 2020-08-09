a959cfc1e7ca68b26ee9bb6f8b964e52eedd8316
Viktor Blomqvist <veblomqvist@gmail.com>
2020-08-09 16:05:38 +0200

    Added today mode file.

---
+++ today-mode.el
@@ -0,0 +1,26 @@
+;;; -*- lexical-binding: t; -*-
+
+
+(defun today-file ()
+  (concat
+   user-emacs-directory
+   (file-name-as-directory "today")
+   ;;(file-name-as-directory "~/.today")
+   (format-time-string "%Y-%m-%d")
+   ".txt"))
+
+(defun today ()
+  (interactive)
+  (find-file-literally (today-file))
+  (delete-other-windows)
+  (today-mode))
+
+(define-derived-mode today-mode text-mode "today"
+  "Major mode for writing about today."
+  (load-fresh-theme 'flatui)
+  (auto-fill-mode 1)
+  ;;(set-window-margins (get-buffer-window) 10)
+  (set-face-background 'fringe (face-background 'default))
+  (setq left-fringe-width 100))
+
+(provide 'today-mode)
